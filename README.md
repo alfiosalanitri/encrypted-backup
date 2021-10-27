4a7dbc27c31066bb4a7f29d7a3044f99f6cbd5ae
alfiosalanitri <alfiosalanitri@gmail.com>
2021-10-27 23:25:18 +0200

    refactoring without rsync usage. Exclude files with -X tar command and make encryption optional.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # encrypted-backup
-how to create an encrypted compressed archive from custom source path to custom path destination. Supports files and folders exclusions from txt file. 
+bash script to create compressed archive from source path to destination.
+Supports files and folders exclusions from txt file and optional encryption with password.
 
 ## Installation
 `chmod +x /path/to/encrypted-baskup.sh`
 
 ## Usage
-`/path/to/encrypted-baskup.sh /path/from/files/ /path/to/destination/ /path/to/excluded.txt`
+- `/path/to/encrypted-baskup.sh /path/from/files/ /path/to/destination/`
+- `/path/to/encrypted-baskup.sh /path/from/files/ /path/to/destination/ /path/to/excluded.txt`
 
 ## How to decrypt
 `gpg -o manual-backup-ddmmYYYY-HHMM.tar.xz -d manual-backup-ddmmYYYY-HHMM.tar.xz.gpg`
--- encrypted-backup.sh
+++ encrypted-backup.sh
@@ -1,61 +1,81 @@
 #!/bin/bash
 #
 # NAME
-# encrypted-baskup.sh - how to create an encrypted compressed archive from custom source path to custom path destination.
-# Supports files and folders exclusions from txt file. 
+# encrypted-baskup.sh - create compressed archive from source path to destination.
+# Supports files and folders exclusions from txt file and encryption with password.
 #
 # SYNOPSIS
-# sudo ./encrypted-baskup.sh /path/from/files/ /path/to/destination/ /path/to/excluded.txt
+# - sudo ./encrypted-baskup.sh /path/from/files/ /path/to/destination/
+# - sudo ./encrypted-baskup.sh /path/from/files/ /path/to/destination/ /path/to/excluded.txt
 #
 # AUTHOR: 
 # backup-website-database.sh is written by Alfio Salanitri www.alfiosalanitri.it and are licensed under the MIT License.
 
+#############################################################
+# Icons	and color
+# https://www.techpaste.com/2012/11/print-colored-text-background-shell-script-linux/
+# https://apps.timwhitlock.info/emoji/tables/unicode
+#############################################################
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[1;33m'
+nocolor='\033[0m'
+icon_ok='\xE2\x9C\x94'
+icon_ko='\xe2\x9c\x97'
+icon_wait='\xE2\x8C\x9B'
+icon_rocket='\xF0\x9F\x9A\x80'
+
 # user input args
 backup_source=$1
 backup_dest=$2
-backup_excluded=$3
-# global args
-backup_temp=/tmp/manual-backup/
 # convert source path to string. Es.: /path/to/Source Dir to path-to-Source-Dir
 filename_prefix=$(echo $backup_source | sed -e 's/\//-/g;s/ /-/g;s/^-\(.*\)-$/\1/')
 
-# check arguments
+# check required arguments
 if [ ! -d "$1" ]; then
-  printf "Backup source directory doesn't exists.\n"
+  printf "[${red}${icon_ko}${nocolor}] Sorry, but ${green}Backup source directory${nocolor} doesn't exists.\n"
   exit 1
 fi
 if [ ! -d "$2" ]; then
-  printf "Backup destination directory doesn't exists.\n"
-  exit 1
-fi
-if [ ! -f "$3" ]; then
-  printf "Backup txt file with excluded files and directory doesn't exists. If no files or folders to exclude, pass an empty txt file.\n"
+  printf "[${red}${icon_ko}${nocolor}] Sorry, but ${green}Backup destination directory${nocolor} doesn't exists.\n" 
   exit 1
 fi
 
-# create temp directory
-sudo mkdir $backup_temp
-
-# sync backup source path to tmp dir
-printf "Rsync started, please wait...\n"
-sudo rsync -zarhL --relative --stats --delete --exclude-from="$backup_excluded" "$backup_source" "$backup_temp"
-
-# compress tmp dir to dest dir
-printf "Rsync completed. Compressing temp dir, please wait...\n"
+sudo printf "\n[${yellow}${icon_wait}${nocolor}] Compressing source path, please wait...\n"
 backup_filename="${backup_dest}${filename_prefix}-$(date +%d%m%Y-%H%M).tar.xz"
-sudo tar cJfP $backup_filename $backup_temp
 
-# remove temp dir
-printf "Compressed archive created. Cleaning...\n"
-sudo rm -r $backup_temp
+# check files or folder to exclude
+if [ -z "$3" ]; then
+  sudo tar cJfP $backup_filename $backup_source
+else
+  # check if excluded file exists
+  if [ ! -f "$3" ]; then
+    printf "[${red}${icon_ko}${nocolor}] Sorry, but ${green}Backup txt file${nocolor} with excluded files and directory doesn't exists.\n"
+    exit 1
+  fi
+  sudo tar cJfP $backup_filename -X $3 $backup_source
+fi
+printf "[${green}${icon_ok}${nocolor}] Source path compressed.\n\n"
+printf "[${yellow}${icon_wait}${nocolor}] Do you want to encrypt the archive?\n"
+echo ""
+PS3="Type 1 or 2: "
+actions=("Yes" "No")
+select action in "${actions[@]}"; do
+  case $action in
+  "Yes")
+    # encrypt archive
+    gpg -c $backup_filename
 
-# encrypt archive
-gpg -c $backup_filename
-
-# remove tar
-sudo rm $backup_filename
-
-# end
-printf "Backup successfully saved.\n"
+    # remove tar
+    sudo rm $backup_filename
+    printf "[${green}${icon_rocket}${nocolor}] Archive encrypted with success.\n"
+    break
+    ;;
+  "No")
+    printf "[${green}${icon_rocket}${nocolor}] Archive saved without encryption.\n"
+    break
+    ;;
+  *) printf "[${red}${icon_ko}${nocolor}] $REPLY isn't a valid option. Try again\n" ;;
+  esac
+done
 exit 1
-~      
